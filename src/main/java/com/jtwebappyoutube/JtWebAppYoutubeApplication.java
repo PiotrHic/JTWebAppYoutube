e1238bb6b69131ccd828aa53169a903319d5110c
piotrhic <piotrhic@gmail.com>
2023-03-06 16:05:47 +0100

    Project init. Closes #1.

---
+++ src/main/java/com/jtwebappyoutube/JtWebAppYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.jtwebappyoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtWebAppYoutubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JtWebAppYoutubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jtwebappyoutube/JtWebAppYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtwebappyoutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtWebAppYoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
