17b03cfe757b25f26bd4f61c08516bb6a7e97fff
piotrhic <piotrhic@gmail.com>
2023-03-07 10:47:28 +0100

    Controller + Thymeleaf dependency + test view. Closes #10.

---
+++ src/main/java/com/jtwebappyoutube/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.jtwebappyoutube.controllers;
+
+import com.jtwebappyoutube.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("allBooks", bookRepository.findAll());
+
+        return "books";
+    }
+}
--- src/main/java/com/jtwebappyoutube/repositories/BookRepository.java
+++ src/main/java/com/jtwebappyoutube/repositories/BookRepository.java
@@ -4,5 +4,6 @@ import com.jtwebappyoutube.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
